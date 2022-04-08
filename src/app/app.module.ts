b7dd03ae66adcee7017348664f79a9888a383ae7
stefanmaric99 <stefan.maric@georgebrown.ca>
2022-04-07 22:58:17 -0400

    user component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+
+import { APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client/core';
+
+const heroku = 'https://airbnb-graphql-yoga.herokuapp.com/';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            // uri: 'http://localhost:4001/graphql',
+            uri: heroku,
+            withCredentials: true,
+          })
+        };
+      },
+      deps: [HttpLink],
+    },
+    // CanActivateRouteService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
